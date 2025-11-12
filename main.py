d3dc329b9aa7a354f07a211e236be25c964b0321
posmith2 <posmith2@illinois.edu>
2025-11-12 17:31:30 -0600

    Modify main.py

--- main.py
+++ main.py
@@ -1,37 +1,75 @@
-import pygame
-from pygame.locals import *
+__version__ = '0.0'
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+try:
+    import json
+    import sys
+    import math
+    import os
+    import pygame as pg
+    from pygame.locals import *
+    from pathlib import Path
+    from datetime import datetime
+    from typing import List, Dict, Any, Optional
+except ImportError as err:
+    print(f"Failed to load module. {err}")
+    sys.exit(2)
 
-circle_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+dir_main = os.path.split(os.path.abspath(__file__))[0]
+dir_saved_scenes = os.path.join(dir_main, "saved_scenes")
+dir_exports = os.path.join(dir_main, "exports")
 
-while running:
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
+#   Resource Acquisition for Startup Menu:
+#       Load list of all saved scenes
+#       Render thumbnails for:
+#        - (2) Two most recently saved scenes
+#        - (1) Autosaved scene (most recently open scene, even if it was closed without saving)
+
+#   Startup Menu (GUI):
+#       - Create new scene
+#       - Load scene
+#       - Import scene
+#         - Recent scenes shown below
+
+#   Scene Editing Loop (GUI):
+#       - Toolbar on left side of screen allows a normal cursor (hold for grabby hand),
+#           Ball placement, Vector placement
+#         - Normal cursor:
+#           - click object with normal cursor (Ball or Path or Vector) to select. Properties tab will display
+#               object info on right side of screen
+#           - hold and drag object with normal cursorto move it somewhere else.
+#           - click on grid with Ball Placement cursor to place a default Ball object
+#           - click and drag on Ball with Vector Placement cursor to place a default Vector object in a specified direction
+#       - File menu on top of screen with save and export options (JSON, mp4) and a Run simulation button
+#           
+
+class BallPath:
+    """Represents the behavior a ball will follow during the simulation"""
+
+    def __init__(self, parent: Ball, end_pos: pg.math.Vector2, start_time: int, end_time: int, path_type="linear"):
+        self.start_pos = Ball.position
+        self.end_pos = end_pos
+        self.start_time = start_time
+        self.end_time = end_time
+        self.path_type = path_type
+        self.layer = -1
+
     
-    screen.fill("gray")
 
-    pygame.draw.circle(screen, "green", circle_pos, 40)
+class Ball:
+    """Represents a single ball in a simulation."""
 
-    keys = pygame.key.get_pressed()
-    if keys[K_w]:
-        circle_pos.y -= 100 * dt
-    if keys[K_s]:
-        circle_pos.y += 100 * dt
-    if keys[K_a]:
-        circle_pos.x -= 100 * dt
-    if keys[K_d]:
-        circle_pos.x += 100 * dt
+    def __init__(self, position: pg.math.Vector2, velocity: pg.math.Vector2,
+                 radius: int, color: Dict[str, Any], layer: int, path: BallPath):
+        self.position = position
+        self.velocity = velocity
+        self.radius = radius
+        self.color_data = color # Color Space -> Color Values e.g. {'space': 'oklab', 'value': [0.7, 0.1, 0.1]}
+        self.layer = layer
 
-    pygame.display.flip()
+        # TODO: This color should be calculated from color_data
+        self.render_color = (0, 0, 0)
 
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
+    def update(self, dt: int):
+        """Update ball's position based on its path."""
+        self.position += 
